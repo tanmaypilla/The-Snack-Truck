e54c7c7c559910525e69dd8dd70ee1c1c1ffb683
tanmaypilla <86410870+tanmaypilla@users.noreply.github.com>
2021-11-21 23:13:00 +0100

    Update vending machine.ino
    Bluetooth application update

--- vending machine.ino
+++ vending machine.ino
@@ -1,20 +1,41 @@
 #include <Servo.h>
 Servo myServo1;
 Servo myServo2;
-int button1 = 2;
-int button2 = 5;
+//int button1 = 2;
+//int button2 = 5;
 
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(button1, INPUT);
-  pinMode(button2, INPUT);
+  //pinMode(button1, INPUT);
+  //pinMode(button2, INPUT);
   myServo1.attach(9);
   myServo2.attach(7);
 }
 
 void loop() {
-  if (digitalRead(button1) == HIGH) {
+  if(Serial.available()>0){
+    char data = Serial.read();
+    switch(data){
+      case 'a':
+      digitalWrite(LED_BUILTIN, HIGH);
+      myServo1.write(180);
+      delay(3000); // can be changed to turn for longer
+
+      break;
+
+      case 'b':
+      digitalWrite(LED_BUILTIN, HIGH);
+      myServo2.write(180);
+      delay(3000); // can be changed to turn for longer
+
+      break;
+
+      default : break;
+    }
+    Serial.println(data);
+  }
+ /* if (digitalRead(button1) == HIGH) {
     digitalWrite(LED_BUILTIN, HIGH);
     myServo1.write(180);
     delay(3000); // can be changed to turn for longer
@@ -33,5 +54,5 @@ void loop() {
     digitalWrite(LED_BUILTIN, LOW);
     myServo2.write(90);
   }
-    
+*/    
 }
